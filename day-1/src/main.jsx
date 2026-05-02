7acecf4eb2b3140d35939d95bf86c0d46c113c64
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-02 21:47:12 +0530

    fix: update main.jsx to wrap JSX elements in a div and include App component

--- day-1/src/main.jsx
+++ day-1/src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import App from "./App";
 // import React from "React"
 
 // const h1 = React.createElement("h1", null , "Hello World! ")
@@ -8,9 +9,13 @@ import { createRoot } from "react-dom/client";
 let name = "shreyesh";
 
 createRoot(document.getElementById("root")).render(
-  <h1 className="h1">Hello World! {name} </h1>, // browser dont know jsx
+  
+  <div>
+    <h1 className="h1">Hello World! {name} </h1>, 
+
+     <h1><App /></h1>
+    {/* browser dont know jsx */}
+  </div>,
 );
-
-
 
 // JSX IS CALLED A SYNTACTICAL SUGAR SINCE IT SHORTENS THE CODE and ensuring ease in writting code.
