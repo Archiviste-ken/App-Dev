7d842b245e6df181dedd3b127f88b97c570725cb
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-02 21:33:38 +0530

    fix: update main.jsx to correct JSX syntax and improve readability

--- day-1/src/main.jsx
+++ day-1/src/main.jsx
@@ -1,8 +1,12 @@
-import { createRoot } from "react-dom/client"
+import { createRoot } from "react-dom/client";
+// import React from "React"
 
+// const h1 = React.createElement("h1", null , "Hello World! ")
+
+// so babel converts the jsx internally the jsx in the form of above line.
+
+let name = "shreyesh";
 
 createRoot(document.getElementById("root")).render(
-
-    <h1>Hello World</h1>
-
-)
+  <h1 className="h1">Hello World! {name} </h1>, // browser dont know jsx
+);
