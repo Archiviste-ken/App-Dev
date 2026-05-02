1b94d9d3ffe38c2e16aa8d5de8a1f3370b08f5e2
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-02 21:39:37 +0530

    fix: add comments to main.jsx for clarity on JSX usage

--- day-1/src/main.jsx
+++ day-1/src/main.jsx
@@ -10,3 +10,7 @@ let name = "shreyesh";
 createRoot(document.getElementById("root")).render(
   <h1 className="h1">Hello World! {name} </h1>, // browser dont know jsx
 );
+
+
+
+// JSX IS CALLED A SYNTACTICAL SUGAR SINCE IT SHORTENS THE CODE and ensuring ease in writting code.
